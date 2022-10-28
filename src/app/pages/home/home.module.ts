0b7f7072a45de65d61008e161865266de8b61b33
Renan Ferro <renan.ferro@fmd.ag>
2022-10-28 11:49:39 -0300

    fix(home module): remove export desnecessário

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -12,9 +12,6 @@ import { HomeComponent } from './home.component';
   imports: [
     CommonModule,
     HomeRoutingModule
-  ],
-  exports: [
-      HomeComponent
   ]
 })
 export class HomeModule { }
