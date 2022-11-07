3cb2238332718d5cdab91b75361de760ea5796e2
Danielton <danielton.ribeiro@fmd.ag>
2022-11-07 07:16:35 -0300

    feat(home): importa componente de video

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { VideoModule } from '../../components/video/video.module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { HomeComponent } from './home.component';
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
-  ]
+    HomeRoutingModule,
+    VideoModule,
+  ],
 })
 export class HomeModule { }
